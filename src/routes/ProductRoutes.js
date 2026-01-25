5f52e4d5edaa91ac32e596d2ead4858797180df0
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-01-25 08:50:45 +0530

    adjust router

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,19 +1,3 @@
-// import express from "express";
-// import {
-//   addProduct,
-//   getProducts
-// } from "../controllers/ProductController.js";
-
-// const router = express.Router();
-
-// // POST: add product
-// router.post("/", addProduct);
-
-// // GET: get all products
-// router.get("/", getProducts);
-// router.get("/:slug", getProductBySlug);
-
-// export default router;
 import express from "express";
 import {
   addProduct,
@@ -33,3 +17,20 @@ router.get("/", getProducts);
 router.get("/:slug", getProductBySlug);
 
 export default router;
+// import express from "express";
+// import {
+//   addProduct,
+//   getProducts
+// } from "../controllers/ProductController.js";
+
+// const router = express.Router();
+
+// // POST: add product
+// router.post("/", addProduct);
+
+// // GET: get all products
+// router.get("/", getProducts);
+// router.get("/:slug", getProductBySlug);
+
+// export default router;
+
