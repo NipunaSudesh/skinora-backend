5a0830e6f5b89dd33b2e6b7c4188623385c78eca
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-01-28 08:33:48 +0530

    created cart  route

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { register, login ,  editProfile,
   logout, } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { authMiddleware  } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.put("/edit-profile", protect, editProfile);
-router.post("/logout", protect, logout);
+router.put("/edit-profile", authMiddleware , editProfile);
+router.post("/logout", authMiddleware , logout);
 
 export default router;
