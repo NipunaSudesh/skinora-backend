e3f79e4c7230ac5cc239221c3e01a9f68601b6e9
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2026-02-10 13:23:32 +0530

    Fix CORS configuration for skinora-vue app

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(
     origin: [
       "http://localhost:3000",
        "http://localhost:5173", 
-      "https://skinora-react.netlify.app"
+      "https://skinora-react.netlify.app",
       "https://skinora-vue.netlify.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
