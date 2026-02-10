4f4ceb9bebe91b6ecc6a448aeb44dcb185815139
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2026-02-10 07:57:26 +0530

    Add localhost:5173 to CORS allowed origins

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+       "http://localhost:5173", 
       "https://skinora-react.netlify.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
